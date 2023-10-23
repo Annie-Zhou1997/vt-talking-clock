ca905407154cc30951e1ea7fa4e2511ad651f4f9
Annie_Z <131166394+Annie-Zhou1997@users.noreply.github.com>
2023-10-24 01:30:27 +0200

    Added a dial

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import sys
 import os
 import pytz
 from PyQt6.QtCore import QTimer, QTime, Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QComboBox, QCheckBox, QComboBox, QStylePainter, QStyle, QStyleOptionComboBox
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QComboBox, QCheckBox, QComboBox, QStylePainter, QStyle, QStyleOptionComboBox, QGridLayout
+from PyQt6.QtGui import QPainter, QPen, QBrush, QColor, QFont
+from PyQt6.QtGui import QRadialGradient
 from gtts import gTTS
 from playsound import playsound
 from datetime import datetime
 from dateutil import tz
 import time
+
 
 class CustomComboBox(QComboBox):
     def paintEvent(self, event):
@@ -21,7 +24,6 @@ class CustomComboBox(QComboBox):
         painter.drawControl(QStyle.ControlElement.CE_ComboBoxLabel, opt)
 
 class TalkingClockApp(QWidget):
-
     def __init__(self):
         super().__init__()
         local_timezone = time.tzname[0]
@@ -34,95 +36,81 @@ class TalkingClockApp(QWidget):
         self.show_time()
 
     def init_ui(self):
-        layout = QVBoxLayout()
+        layout = QGridLayout(self)
+
+        # 在这里创建 ClockWidget 的实例并将其添加到布局中
+        self.clock_widget = ClockWidget(self)
+        layout.addWidget(self.clock_widget, 0, 0, 1, 3)
+        # 设置窗口标题和大小
+        self.setWindowTitle('Talking Clock')
+        self.setGeometry(100, 100, 600, 600)
+
+        # 电子时钟标签
         self.label = QLabel(self)
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.label.setStyleSheet("QLabel { font-size: 40px; }")
-        layout.addWidget(self.label)
+        self.label.setGeometry(150, 345, 300, 50)  # 设置电子时钟的位置和大小
 
         # English button
-        h_layout_en = QHBoxLayout()
         self.play_button = QPushButton('English', self)
-        self.play_button.setMinimumWidth(95)  # 设置最大宽度
+        self.play_button.setGeometry(250, 400, 100, 30)  # 设置按钮的位置和大小
         self.play_button.clicked.connect(self.play_time_en)
-        h_layout_en.addStretch()
-        h_layout_en.addWidget(self.play_button)
-        h_layout_en.addStretch()
-        layout.addLayout(h_layout_en)
 
         # Chinese button
-        h_layout_zh = QHBoxLayout()
         self.play_button_zh = QPushButton('中文', self)
-        self.play_button_zh.setMinimumWidth(95)  # 设置最大宽度
+        self.play_button_zh.setGeometry(250, 450, 100, 30)  # 设置按钮的位置和大小
         self.play_button_zh.clicked.connect(self.play_time_zh)
-        h_layout_zh.addStretch()
-        h_layout_zh.addWidget(self.play_button_zh)
-        h_layout_zh.addStretch()
-        layout.addLayout(h_layout_zh)
 
         # Russian button
-        h_layout_ru = QHBoxLayout()
         self.play_button_ru = QPushButton('Русский', self)
-        self.play_button_ru.setMinimumWidth(95)  # 设置最大宽度
+        self.play_button_ru.setGeometry(250, 500, 100, 30)  # 设置按钮的位置和大小
         self.play_button_ru.clicked.connect(self.play_time_ru)
-        h_layout_ru.addStretch()
-        h_layout_ru.addWidget(self.play_button_ru)
-        h_layout_ru.addStretch()
-        layout.addLayout(h_layout_ru)
 
         # Dutch button
-        h_layout_nl = QHBoxLayout()
         self.play_button_nl = QPushButton('Nederlands', self)
-        self.play_button_nl.setMinimumWidth(95)  # 设置最大宽度
+        self.play_button_nl.setGeometry(250, 550, 100, 30)  # 设置按钮的位置和大小
         self.play_button_nl.clicked.connect(self.play_time_nl)
-        h_layout_nl.addStretch()
-        h_layout_nl.addWidget(self.play_button_nl)
-        h_layout_nl.addStretch()
-        layout.addLayout(h_layout_nl)
 
         # Timezone combo box
         self.timezone_combo = CustomComboBox(self)
-        self.timezone_combo.addItem("Select Other Timezone")  # default
+        self.timezone_combo.addItem("Select Other Timezone")
         for tz in pytz.all_timezones:
             self.timezone_combo.addItem(tz)
+        self.timezone_combo.setGeometry(10, 10, 190, 30)  # 设置组合框的位置和大小
         self.timezone_combo.currentIndexChanged.connect(self.update_timezone)
-        layout.addWidget(self.timezone_combo)
 
         # 24 hour format checkbox
         self.format_checkbox = QCheckBox('24 Hour Format', self)
+        self.format_checkbox.setGeometry(20, 560, 200, 30)  # 设置复选框的位置和大小
         self.format_checkbox.stateChanged.connect(self.update_format)
-        layout.addWidget(self.format_checkbox)
-        self.setLayout(layout)
-        self.setGeometry(100, 100, 300, 200)
-        self.setWindowTitle('Talking Clock')
+
         self.setStyleSheet("""
             QWidget {
                 background: qlineargradient(x1:0, y1:0, x2:1, y2:1,
-                                            stop:0 #fbc2eb, stop:1 #a6c1ee);  /* 主背景渐变色 Основной фон градиентного цвета*/
+                                            stop:0 #fbc2eb, stop:1 #a6c1ee);
             }
             QPushButton {
-                border: 2px solid #8f8f91;  /* 按钮边框颜色 Цвет границы кнопки */
-                border-radius: 10px;  /* 按钮圆角大小 Размер закругления кнопки */
+                border: 2px solid #8f8f91;
+                border-radius: 10px;
                 background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-                                                  stop: 0 #f6f7fa, stop: 1 #dadbde);  /* 按钮背景渐变色 Цвет фонового градиента кнопки */
+                                                  stop: 0 #f6f7fa, stop: 1 #dadbde);
             }
             QPushButton:pressed {
                 background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-                                                  stop: 0 #dadbde, stop: 1 #f6f7fa);  /* 按钮被按下时的背景渐变色 */
+                                                  stop: 0 #dadbde, stop: 1 #f6f7fa);
             }
             QLabel, QComboBox, QCheckBox {
-                background: transparent;  /* 标签、组合框和复选框背景透明 */
-                border: none;  /* 无边框 */
+                background: transparent;
+                border: none;
             }
             QLabel {
-                font-family: 'PixelArmy';  /* 电子时钟字体 */
-                font-size: 40px;  /* 字体大小 */
+                font-family: 'PixelArmy';
+                font-size: 40px;
             }
             QComboBox QAbstractItemView {
-                background-color: #e2ebf0;  /* 设置下拉菜单打开后的项的背景颜色 */
+                background-color: #e2ebf0;
             }
         """)
-
 
     def show_time(self):
         selected_timezone = self.timezone_combo.currentText()
@@ -172,13 +160,92 @@ class TalkingClockApp(QWidget):
 
     def update_timezone(self):
         selected_timezone = self.timezone_combo.currentText()
-        self.timezone = pytz.timezone(selected_timezone)
+        if selected_timezone == 'Select Other Timezone':
+            self.timezone = tz.tzlocal()
+        else:
+            self.timezone = pytz.timezone(selected_timezone)
 
     def update_format(self):
         self.format_24hr = self.format_checkbox.isChecked()
+
+    def update_timezone(self):
+        selected_timezone = self.timezone_combo.currentText()
+        if selected_timezone == 'Select Other Timezone':
+            self.timezone = tz.tzlocal()
+        else:
+            self.timezone = pytz.timezone(selected_timezone)
+        self.clock_widget.set_timezone(self.timezone)  # 更新钟表表盘的时区
+
+class ClockWidget(QWidget):
+    def __init__(self, parent=None):
+        super(ClockWidget, self).__init__(parent)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(1000)
+        self.timezone = tz.tzlocal()  # 默认使用本地时区
+
+    def set_timezone(self, timezone):
+        self.timezone = timezone
+        self.update()  # 立即更新钟表表盘
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.begin(self)
+        self.draw_clock(painter)
+        painter.end()
+
+    def draw_clock(self, painter):
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        center_x = self.width() / 2
+        center_y = self.height() / 3.5
+        clock_radius = min(self.width(), self.height()) / 4
+        painter.translate(center_x, center_y)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        # 绘制表盘
+        gradient = QRadialGradient(0, 0, clock_radius)
+        gradient.setColorAt(0, QColor(226, 235, 240))
+        gradient.setColorAt(1, QColor(207, 217, 233))
+        painter.setBrush(QBrush(gradient))
+        painter.setPen(QPen(QColor(0, 0, 0), 2))
+        painter.drawEllipse(int(-clock_radius), int(-clock_radius), int(2 * clock_radius), int(2 * clock_radius))
+
+        # 绘制刻度
+        for i in range(12):
+            painter.drawLine(0, round(-clock_radius), 0, round(-clock_radius + 10))
+            painter.rotate(30)
+
+        # 获取当前时间
+        current_time = datetime.now(self.timezone).time()
+        secs = current_time.second
+        mins = current_time.minute + secs / 60
+        hrs = current_time.hour + mins / 60
+
+        # 绘制时针
+        painter.setPen(QPen(QColor(50, 50, 50), 6))
+        painter.setBrush(QColor(50, 50, 50))
+        painter.rotate(hrs * (360 / 12))
+        painter.drawRect(-5, int (-clock_radius / 2), 10, int(clock_radius / 2))  # 使用矩形作为时针
+        painter.rotate(-hrs * (360 / 12))
+
+        # 绘制分针
+        painter.setPen(QPen(QColor(0, 0, 0), 4))
+        painter.rotate(mins * (360 / 60))
+        painter.drawLine(0, 0, 0, round(-clock_radius * 0.7))
+        painter.rotate(-mins * (360 / 60))
+
+        # 绘制秒针
+        painter.setPen(QPen(QColor(255, 0, 0), 2))
+        painter.rotate(secs * (360 / 60))
+        painter.drawLine(0, 0, 0, round(-clock_radius * 0.9))
+        painter.rotate(-secs * (360 / 60))
+
+        painter.restore()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = TalkingClockApp()
     ex.show()
+    ex.setGeometry(100, 100, 600, 600)
     sys.exit(app.exec())
